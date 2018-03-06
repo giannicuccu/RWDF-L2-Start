1a4aa8efac91567e6647f0092bbf9f84fc91c6f8
Gianni <gianni.cuccu@gmail.com>
2018-03-06 23:44:55 +0100

    clean build packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,1 @@
 
-var gulp = require('gulp');
-var inlineimg = require('gulp-inline-image');
- 
-gulp.task('default', function () {
-  gulp.src('html_src/**/*.html')
-    .pipe(inlineimg('images_src'))  // takes in the directory to use as the root when looking for images
-    .pipe(gulp.dest('./'));
-});
